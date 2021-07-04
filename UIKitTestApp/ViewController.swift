91b15b76c9c9a6a9d0372f7751c791ba65b45b2a
Sawwaty <yezhovye@mac.com>
2021-07-04 14:11:45 +0300

    Initial Commit

---
+++ UIKitTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTestApp
+//
+//  Created by Victoria Klimova on 04.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
