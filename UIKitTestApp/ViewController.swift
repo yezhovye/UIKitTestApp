aef155e5594e57759c360a7ad9891d51a1343b5d
Sawwaty <yezhovye@mac.com>
2021-07-04 18:07:01 +0300

    UI elements are defined, their  initial properties are defined

--- UIKitTestApp/ViewController.swift
+++ UIKitTestApp/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        messageLabel.font = messageLabel.font.withSize(35)
+        messageLabel.textAlignment = .center
+        messageLabel.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
+        
+        messageLabel.text = String(slider.value)
+        
+        datePicker.locale = Locale(identifier: "ru_RU")
+        
+        
+        
     }
 
 
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+    }
 }
 
